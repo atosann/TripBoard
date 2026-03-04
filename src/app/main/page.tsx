7686aaf5e0ce7dd7f483d52ed051fde6856d13be
atosann <gggooo2438@gmail.com>
2026-03-04 18:26:08 +0900

    fix register link

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -149,7 +149,7 @@ export default function MainPage() {
                         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
-                        詳細・参加する（要ログイン）
+                        詳細・参加する
                       </button>
                     </div>
                   </div>
